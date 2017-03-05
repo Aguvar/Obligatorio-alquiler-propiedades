7775aea39e70466f5f6e712894c4fcc45995fc65
Dingo <gustivarela@hotmail.com>
2017-03-05 20:14:32 -0300

    Implementando la funcion de promedios en la descripcion del producto
    La consulta se esta devolviendo vacia por ahora

--- single.php
+++ single.php
@@ -26,20 +26,44 @@ $conn = new ConexionBD('mysql', SERVER, BD_INMOBILIARIA, USUARIO_BD, CLAVE_BD);
 
 if ($conn) {
     $conn->conectar();
-    
+
     $sql = 'SELECT barrios.nombre AS barrio, propiedades.precio, propiedades.titulo, propiedades.mts2, propiedades.habitaciones, propiedades.banios, propiedades.garage, propiedades.texto
             FROM propiedades
             INNER JOIN barrios ON barrios.id = propiedades.barrio_id
             WHERE propiedades.id = :idCasa';
 
     $parametros = array(
-        array('idCasa',$idCasa,'int',0)
+        array('idCasa', $idCasa, 'int', 0)
     );
 
-    if ($conn->consulta($sql,$parametros)) {
+    if ($conn->consulta($sql, $parametros)) {
 
-//        print_r($conn->restantesRegistros());
-        $mySmart->assign('info', $conn->restantesRegistros());
+        $infoCasa = $conn->restantesRegistros();
+
+        $mts = $infoCasa[0]["mts2"];
+        $precio = $infoCasa[0]["precio"];
+        $barrio = $infoCasa[0]["barrio"];
+
+        $mySmart->assign('info', $infoCasa);
+    } else {
+        echo 'consulta not ok';
+    }
+
+    //Precio/mts2
+    $promedioCasa = $precio / $mts;
+
+    $mySmart->assign("promedioCasa", $promedioCasa);
+
+    $sql = "SELECT AVG(precio)/AVG(mts2) AS promedio FROM `propiedades` WHERE precio != 0 AND barrio_id = :idBarrio";
+
+    $parametros = array(
+        array('idBarrio', (int)$barrio, 'int', 0)
+    );
+
+    if ($conn->consulta($sql, $parametros)) {
+    
+        print_r($conn->restantesRegistros());
+        
     } else {
         echo 'consulta not ok';
     }
